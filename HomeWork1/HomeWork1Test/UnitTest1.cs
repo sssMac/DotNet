6108eb286b12e1aa91ba48b221f1e362dc984707
Ilya <ilya-lyapin@mail.ru>
2021-10-11 19:49:10 +0300

    Chages unit test

--- HomeWork1/HomeWork1Test/UnitTest1.cs
+++ HomeWork1/HomeWork1Test/UnitTest1.cs
@@ -6,36 +6,57 @@ namespace HomeWork1Test
     [TestClass]
     public class UnitTest1
     {
+
         [TestMethod]
-        public void ProgramTests()
+        public void Sum_20_Plus_5_25_Returned()
         {
-            string[][] args = new string[][]
-            {
-                new string[] {"20", "+", "5"},
-                new string[] {"20", "-", "5"},
-                new string[] {"20", "*", "5"},
-                new string[] {"20", "/", "5"},
-                new string[] {"a", "/", "5"},
-                new string[] {"20", "?", "5"},
-
-            };
-
-            for (int i = 0; i < 4; i++)
-            {
-                var expected0 = Program.Main(args[i]);
-                Assert.AreEqual(expected0,0);
-            }
-            
-            var expected1 = Program.Main(args[4]);
-            Assert.AreEqual(expected1,1);
-            
-            var expected2 = Program.Main(args[5]);
-            Assert.AreEqual(expected2, 2);
-            
+            string[] args = new string[] { "20", "+", "5" };
+            var expected = Program.Main(args);
+            Assert.AreEqual(expected, 0);
         }
-        
+
         [TestMethod]
-        public void CalculatorNullResaultTests()
+        public void Dif_20_Minus_5_15_Returned()
+        {
+            string[] args = new string[] { "20", "-", "5" };
+            var expected = Program.Main(args);
+            Assert.AreEqual(expected, 0);
+        }
+
+        [TestMethod]
+        public void Prod_20_Multy_5_100_Returned()
+        {
+            string[] args = new string[] { "20", "*", "5" };
+            var expected = Program.Main(args);
+            Assert.AreEqual(expected, 0);
+        }
+
+        [TestMethod]
+        public void Priv_20_Divide_5_4_Returned()
+        {
+            string[] args = new string[] { "20", "/", "5" };
+            var expected = Program.Main(args);
+            Assert.AreEqual(expected, 0);
+        }
+
+        [TestMethod]
+        public void Error_5_divide_a_1_Returned()
+        {
+            string[] args = new string[] { "5", "/", "a" };
+            var expected = Program.Main(args);
+            Assert.AreEqual(expected, 1);
+        }
+
+        [TestMethod]
+        public void Erorr_20_symbol_5_2_Returned()
+        {
+            string[] args = new string[] { "20", "?", "5" };
+            var expected = Program.Main(args);
+            Assert.AreEqual(expected, 2);
+        }
+
+        [TestMethod]
+        public void CalculatorNullResultTests()
         {
             Calculator calc = new Calculator();
 
@@ -44,10 +65,9 @@ namespace HomeWork1Test
             string operation = null;
 
             var res = calc.Calculate(val1, operation, val2);
-            
-            Assert.AreEqual(res,0);
+
+            Assert.AreEqual(res, 0);
         }
-        
+
     }
-    
 }
